0053c5e37077550217457ed47834ead28b83b2f9
marucmiralova <marucmiralova@seznam.cz>
2025-04-07 09:17:29 +0200

    linerani vyhledavani

--- searching.py
+++ searching.py
@@ -22,11 +22,51 @@ def read_data(file_name, field):
 
     return sequential_data[field]
 
+#naivni algoritmus
+def linear_search(seq, number):
+    """
+    Implementujte algoritmus sekvenčního vyhledávání, který v neseřazeném seznamu nalezne
+    pozice a četnost výskytu zadaného čísla.
+    ● V modulu searching.py vytvořte funkci linear_search(). Funkce bude mít dva
+    vstupní parametry – prohledávanou sekvenci a hledané číslo.
+    ● Funkce vrátí slovník se dvěma klíči. Pod prvním klíčem positions bude uložen
+    seznam pozic (indexů). Pod druhým klíčem count bude uložen počet výskytů
+    hledaného čísla.
+    ● Volání funkce a korektnost její implementace ověřte voláním z hlavní funkce main().
+    V hlavní funkci definujte také vyhledávané číslo.
+    ● Vytvořte novou revizi (commit) a změny nahrajte na svůj vzdálený repozitář (push).
+    :param sequence:
+    :param number:
+    :return:
+    """
+    # seznam_pozic = []
+    # for idx, cislo in enumerate(sequence):
+    #     if cislo == number:
+    #         seznam_pozic.append(idx)
+    #     else:
+    #         continue
+    # pocet_vyskytu = len(seznam_pozic)
+    # slovnik = {"positions":seznam_pozic, "count":pocet_vyskytu}
+    # return slovnik
+    indices = []
+    count = 0
+    idx = 0
+    while idx < len(seq):
+        if seq[idx] == number:
+            indices.append(idx)
+            count += 1
+        idx += 1
+
+    return {"positions":indices, "count":count,}
+
+
 
 def main():
     file_name = "sequential.json"
     seq = read_data(file_name, field="unordered_numbers")
     print(seq)
+    vyhledavani = linear_search(seq, 0)
+    print(vyhledavani)
 
 
 
